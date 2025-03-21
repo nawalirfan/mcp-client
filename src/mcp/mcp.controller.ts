be81a5eb1f4b34207da39ad720337f72fb7d0c53
nawalirfan <nawal.irfan1110@gmail.com>
2025-03-21 12:22:59 +0500

    mcp client setup

---
+++ src/mcp/mcp.controller.ts
@@ -0,0 +1,19 @@
+// import { Controller, Get, Post, Res, Req } from '@nestjs/common';
+// import { Response, Request } from 'express';
+// import { McpService } from './mcp.service';
+
+// @Controller()
+// export class McpController {
+//   private transport: StdioServerTransport | null = null;
+
+//   constructor(private readonly mcpService: McpService) {}
+
+//   // @Get('/sse')
+
+//   // async connectSSE(@Res() res: Response) {
+//   //   this.transport = new StdioServerTransport();
+//   //   await this.mcpService.getServer().connect(this.transport);
+//   // }
+
+  
+// }
---
+++ src/mcp/mcp.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@nestjs/common';
+import { Server } from '@modelcontextprotocol/sdk/server/index.js';
+import { Client } from '@modelcontextprotocol/sdk/client/index.js';
+import {
+  ListToolsRequestSchema,
+  CallToolRequestSchema,
+} from '@modelcontextprotocol/sdk/types.js';
+import { z } from 'zod';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js';
+import { zodToJsonSchema } from 'zod-to-json-schema';
+
+@Injectable()
+export class McpService {
+  private readonly client: Client;
+  private readonly transport: StdioClientTransport;
+
+  constructor() {
+    this.transport = new StdioClientTransport({
+      command: 'node',
+      args: ['dist/main.js'],
+      cwd: 'C:/Users/irfan/Desktop/EdgeFirm/mcp/mcp-app',
+    });
+    this.client = new Client(
+      {
+        name: 'example-client',
+        version: '1.0.0',
+      },
+      {
+        capabilities: {
+          prompts: {},
+          resources: {},
+          tools: {},
+        },
+      },
+    );
+  }
+
+  async onModuleInit() {
+    await this.client.connect(this.transport);
+    console.log('✅ MCP Client Connected...');
+    this.listTools();
+    this.callTool('scan_nmap', {
+      target: 'localhost',
+      scanType: 'quick',
+      timing: 3,
+    });
+  }
+
+  async listTools() {
+    const tools = await this.client.listTools();
+    console.log('🔧 Available Tools:', tools);
+    return tools;
+  }
+
+  async callTool(name: string, args: Record<string, any>) {
+    try {
+      const result = await this.client.callTool({ name, arguments: args });
+      console.log(`✅ Tool Executed: ${name}`, result);
+      return result;
+    } catch (error) {
+      console.error(`❌ Error executing tool: ${name}`, error);
+      throw error;
+    }
+  }
+
+  getClient(): Client {
+    return this.client;
+  }
+}
